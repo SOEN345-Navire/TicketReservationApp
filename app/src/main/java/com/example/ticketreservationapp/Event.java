53ba8281d2204bd516050d2af06675728d491a73
valerie-nguyen <val.nguyen08@gmail.com>
2026-03-29 21:09:17 -0400

    Create Event class

---
+++ app/src/main/java/com/example/ticketreservationapp/Event.java
@@ -0,0 +1,82 @@
+package com.example.ticketreservationapp;
+
+import com.google.firebase.Timestamp;
+
+public class Event {
+    private String id;
+    private String name;
+    private Timestamp date;
+    private String location;
+    private String category;
+    private int reservedPlaces;
+    private int maxPlaces;
+
+    public Event() {
+
+    }
+
+    public Event(String name, Timestamp date, String location, String category, int reservedPlaces, int maxPlaces) {
+        this.name = name;
+        this.date = date;
+        this.location = location;
+        this.category = category;
+        this.reservedPlaces = reservedPlaces;
+        this.maxPlaces = maxPlaces;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Timestamp getDate() {
+        return date;
+    }
+
+    public void setDate(Timestamp date) {
+        this.date = date;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getReservedPlaces() {
+        return reservedPlaces;
+    }
+
+    public void setReservedPlaces(int reservedPlaces) {
+        this.reservedPlaces = reservedPlaces;
+    }
+
+    public int getMaxPlaces() {
+        return maxPlaces;
+    }
+
+    public void setMaxPlaces(int maxPlaces) {
+        this.maxPlaces = maxPlaces;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
